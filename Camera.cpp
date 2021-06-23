936f7576183305531d3b8ce48f3b61360bc50339
1 <1>
2021-06-23 14:49:21 +0600

    multi thread render

--- Camera.cpp
+++ Camera.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <thread>
 
 struct Camera{
     int hsize, vsize;
@@ -40,17 +41,45 @@ struct Camera{
     }
 };
 
-Canvas render(Camera& cam, World& world){
-    Canvas image = Canvas(cam.hsize, cam.vsize);
-    for (int y = 0; y < cam.vsize; y++){
-        for (int x = 0; x < cam.hsize; x++){
-            Ray ray = cam.ray_for_pixel(x, y);
-            Color color = color_at(world, ray);
-            write_pixel(image, x, y, color);
-        }
-        //std::cout << "\rProgress: " << (y * 100) / cam.vsize << "%" << std::flush;
-        std::cout << "\rLine: " << y+1 << "/" << cam.vsize << std::flush;
+void render_thread(Canvas *image, Camera *cam, World *w, const int &mod) {
+  for (int y = 0; y < cam->vsize; ++y) {
+    if (y % std::thread::hardware_concurrency() != mod) {
+      continue;
     }
-    std::cout << "\nDone!" << std::endl;
-    return image;
+    for (int x = 0; x < cam->hsize; ++x){
+      Ray r = cam->ray_for_pixel(x, y);
+      Color c = color_at(*w, r);
+      write_pixel(*image, x, y, c);
+    }
+  }
 }
+
+Canvas render(Camera& cam, World& world){
+    std::cout << "Rendering with " << std::thread::hardware_concurrency()
+    << " threads." << std::endl;
+    Canvas image = Canvas(cam.hsize, cam.vsize);
+    std::vector<std::thread> threads;
+    for (int i = 0; i < std::thread::hardware_concurrency(); ++i) {
+        std::thread thread(&render_thread, &image, &cam, &world, i);
+        threads.push_back(std::move(thread));
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
+    return image;
+
+
+    // for (int y = 0; y < cam.vsize; y++){
+    //     for (int x = 0; x < cam.hsize; x++){
+    //         Ray ray = cam.ray_for_pixel(x, y);
+    //         Color color = color_at(world, ray);
+    //         write_pixel(image, x, y, color);
+    //     }
+    //     //std::cout << "\rProgress: " << (y * 100) / cam.vsize << "%" << std::flush;
+    //     std::cout << "\rLine: " << y+1 << "/" << cam.vsize << std::flush;
+    // }
+    // std::cout << "\nDone!" << std::endl;
+    // return image;
+}
+
+
